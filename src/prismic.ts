93cbbf68bfc352e03858060e28cce08e81cd72d4
Diegohofs <diegohofs@gmail.com>
2022-10-28 08:23:26 -0300

    change access token value

--- src/prismic.ts
+++ src/prismic.ts
@@ -5,7 +5,7 @@ export const repositoryName = 'casafac'
 
 export const client = prismic.createClient(repositoryName, {
   // If your repository is private, add an access token
-  accessToken: 'MC5ZMXBHSGhRQUFDWUFjbzdP.77-977-9QhQo77-977-9GWrvv73vv70WEu-_ve-_vQF377-9dmDvv73vv73vv70t77-9f--_ve-_ve-_ve-_vVo1',
+  accessToken: '',
 
   // This defines how you will structure URL paths in your project.
   // Update the types to match the Custom Types in your project, and edit
